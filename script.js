74f123536a37070f4cc04ecad98db3b0ef3c6ef8
mohammed abdu rahoof <mohammedabdurahoof527@gmail.com>
2021-01-11 06:27:26 -0800

    calculator model

---
+++ script.js
@@ -0,0 +1,11 @@
+function buttonclick(val) {
+    document.getElementById("screen").value += val;
+}
+function clearscreen(){
+    document.getElementById("screen").value=""
+}
+function equalClick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
